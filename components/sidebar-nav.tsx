1ad480c2fd0d72e70b1c44b345f48f15e9a9834d
rehlatoursid-umrah <rehlatours.id@gmail.com>
2025-11-05 11:43:55 +0200

    Update sidebar-nav.tsx

--- components/sidebar-nav.tsx
+++ components/sidebar-nav.tsx
@@ -4,7 +4,7 @@ import Image from "next/image"
 import { Roboto } from "next/font/google"
 
 const roboto = Roboto({
-  weight: ["1000"], // Bold
+  weight: ["900"], // Bold
   subsets: ["latin"],
 })
 
