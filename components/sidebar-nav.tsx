8f4a4fecf37938102de87a7a10397de25883de99
rehlatoursid-umrah <rehlatours.id@gmail.com>
2025-11-05 11:40:11 +0200

    Update sidebar-nav.tsx

--- components/sidebar-nav.tsx
+++ components/sidebar-nav.tsx
@@ -24,17 +24,15 @@ export function SidebarNav() {
   return (
     <div className="fixed left-0 top-0 h-screen w-64 bg-background border-r border-border overflow-y-auto pt-6">
       {/* LOGO */}
-      <div className="px-6 mb-4 flex justify-center">
-        <div className="w-40 h-40 flex items-center justify-center">
-          <Image
-            src="/rem.png"
-            alt="Logo Rehlatours"
-            width={160}
-            height={160}
-            className="object-contain w-full h-full"
-            priority
-          />
-        </div>
+      <div className="px-6 mb-2 flex justify-center">
+        <Image
+          src="/rem.png"
+          alt="Logo Rehlatours"
+          width={140}     // ukuran logo besar
+          height={140}
+          className="object-contain"
+          priority
+        />
       </div>
 
       {/* NAVIGATION */}
